659430427dd966cbb684044c1d23f2cf89360ec2
Florencia Buenavida <buenavidafarhy@gmail.com>
2024-11-13 10:12:23 -0300

    Sass, tienda, css

---
+++ trabajo.final-main/script.js
@@ -0,0 +1,108 @@
+/*function saludo() {
+    console.log("¡Hola! Sean bienvenidos a la consola de la página oficial de Coldplay"); }
+
+saludo();
+
+alert("¡Hola! Les damos la bienvenida a nuestra página. Queríamos solicitarles que contesten una breve encuenta sobre nuestro último album.")
+
+let autorizacion = confirm ("Confirma que acepta los términos y condiciones.")
+
+
+let usuario = prompt ("Nombre y Apellido")
+let respuesta = prompt (usuario  + "¿Te gustó la nueva canción 'feelslikeimfallinginlove'?"); {alert("Respuesta enviada. Muchas gracias.")}
+
+const intro = "El usuario se llama "+ usuario;
+const respuestacompleta = usuario + " dijo que " + respuesta + " le gustó la canción 'feelslikeimfallinginlove'.";
+
+
+function mensaje(usuario, respuesta){
+    console.log (intro);
+    console.log(respuestacompleta);
+    
+}
+
+mensaje();
+
+
+
+/* Iniciamos los contadores*/
+
+/*Para el recuento de votos - Supongamos que tenemos estos votos */
+/*let votos = ["Si", "si", "no", "No", "si"];
+recuentoDeVotos (votos);
+
+function recuentoDeVotos (votos){
+    let votospositivos = 0;
+    let votosnegativos = 0;
+    
+    
+ for (let i = 0; i< votos.length; i++
+ ) {
+  if (votos[i].toLowerCase () === "si") { votospositivos++;} 
+  else if (votos[i].toLowerCase () === "no" ){votosnegativos++;}
+  }
+
+  console.log ("Votos a favor: " + votospositivos);
+  console.log ("Votos en contra: " + votosnegativos);
+}
+
+recuentoDeVotos(votos);
+
+/*productos*/
+
+/*const productos = ['buzo1', 'buzo2', 'buzo3', 'remera1', 'remera2', 'remera3'];
+productos.push('disco Feelslikeimfallinginlove');
+
+function tienda (){
+    console.log("En la tienda están disponibles los siguientes productos: " + productos);
+}
+tienda();
+
+const buzo1 ={
+Nombre:'Buzo 1 - Coldplay',
+Color: 'Azul',
+Precio: '20000',
+}
+
+const buzo2 = {
+Nombre: 'Buzo 2 - We Pray',
+Color: 'Negro',
+Precio: '25000',
+}
+
+const buzo3 = {
+Nombre: 'Buzo 3 - Coldplay',
+Color: 'Blanco',
+Precio: '20000',
+}
+
+const remera1 = {
+Nombre: 'Remera 1',
+Color: 'Azul',
+Precio: '15000',
+}
+
+const remera2 ={
+Nombre: 'Remera 2 - We pray',
+Color:'Negro',
+Precio: '15000',
+}
+
+const remera3 ={
+Nombre: 'Remera 3 - Coldplay',
+Color: 'Blanco',
+Precio: '15000',
+}
+/*array es la lista con todos los productos del mercado*/
+
+
+
+
+
+
+
+
+
+
+
+
